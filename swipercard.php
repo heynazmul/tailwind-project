4d542ac0649eac70e9986897dd8c81ef027b91a6
Nazmul Hossain <heynazmulhossain@gmail.com>
2023-03-12 20:12:50 +0600

    update

--- swipercard.php
+++ swipercard.php
@@ -2,31 +2,32 @@
 	<div class="containar">
 		<div id="swipercard" class="swiper ">
 			<div class="swiper-wrapper">
-			  <div class="swiper-slide"><div class="w-72 h-fit group">
-				<div class="relative">
-					<div>
-						<span class="bg-blue-500 text-slate-100 ml-auto">50%off</span>
-					</div>
-					<div class="relative h-96 w-72">
-					  <img class="absolute h-96 ease-in opacity-100 group-hover:opacity-0 transition duration-700 left-0 bottom-0  w-full object-cover" src="images/demo/product-01.png" alt="">
-					  <img class="absolute  h-96 opacity-0 group-hover:opacity-100 left-0 bottom-0 w-full transition duration-700" src="images/demo/product-08.png" alt="">
-					</div>
-					<div class="absolute h-full w-full  flex items-end justify-center mb-10 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-700">
-						<div class="cursor-pointer p-2 hover:p-3 bg-red-300 m-1 hover:m-0 rounded transition-all duraiton-700">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" h-6 w-6">
-							  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-							  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+			  <div class="swiper-slide">
+				<div class="w-72 h-fit group">
+					<div class="relative">
+						<div>
+							<span class="bg-blue-500 text-slate-100 ml-auto">50%off</span>
+						</div>
+						<div class="relative h-96 w-72">
+						  <img class="absolute h-96 ease-in opacity-100 group-hover:opacity-0 transition duration-700 left-0 bottom-0  w-full object-cover" src="images/demo/product-01.png" alt="">
+						  <img class="absolute  h-96 opacity-0 group-hover:opacity-100 left-0 bottom-0 w-full transition duration-700" src="images/demo/product-08.png" alt="">
+						</div>
+						<div class="absolute h-full w-full  flex items-end justify-center mb-10 -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-700">
+							<div class="cursor-pointer p-2 hover:p-3 bg-red-300 m-1 hover:m-0 rounded transition-all duraiton-700">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" h-6 w-6">
+								  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+								  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+								</svg>
+							</div>
+							<button class="bg-red-300 text-white rounded my-1 mx-2 px-6 py-2 hover:m-0 hover:px-7 hover:py-3 transition-all duraiton-700">Add to cart</button>
+							<div class="cursor-pointer p-2 hover:p-3 bg-red-300 m-1 hover:m-0 rounded transition-all duraiton-700">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+							  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
 							</svg>
+							</div>
 						</div>
-						<button class="bg-red-300 text-white rounded my-1 mx-2 px-6 py-2 hover:m-0 hover:px-7 hover:py-3 transition-all duraiton-700">Add to cart</button>
-						<div class="cursor-pointer p-2 hover:p-3 bg-red-300 m-1 hover:m-0 rounded transition-all duraiton-700">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-						  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
-						</svg>
-						</div>
+						
 					</div>
-					
-				</div>
 				<div>
 					<div class="flex flex-wrap">
 						 <svg xmlns="http://www.w3.org/2000/svg" fill="#fcd34d" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fcd34d" class=" w-4 h-4">
